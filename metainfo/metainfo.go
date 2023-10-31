e12eafd0689d0252647987aabe9f34d4a20dd481
Marina Sakai <118230951+Marina-Sakai@users.noreply.github.com>
2023-10-31 10:22:38 +0800

    feat: support multi-service for ReportMetaInfo (#7)
    * feat: GetServiceInfo return value change
    
    * chore: upgrade versions

--- metainfo/metainfo.go
+++ metainfo/metainfo.go
@@ -75,78 +75,86 @@ func NewMetaReporter(c *util.OpenSergoConfig) (*OpenSergoMetaReporter, error) {
 }
 
 // ReportMetaInfo report meta info to opensergo
-func (o *OpenSergoMetaReporter) ReportMetaInfo(srvInfo *serviceinfo.ServiceInfo) error {
+func (o *OpenSergoMetaReporter) ReportMetaInfo(svcs map[string]*serviceinfo.ServiceInfo) error {
 	if !o.enable {
 		klog.Warn(util.ErrConfigNotFound.Error())
 		return util.ErrConfigNotFound
 	}
-	metaReq, err := o.openSergoMetaReq(srvInfo)
+	metaReqs, err := o.openSergoMetaReq(svcs)
 	if err != nil {
 		klog.Errorf("err:%+v", err)
 		return err
 	}
-	if _, err = o.client.ReportMetadata(context.TODO(), metaReq); err != nil {
-		klog.Errorf("err:%+v", err)
-		return err
+	for _, metaReq := range metaReqs {
+		if _, err = o.client.ReportMetadata(context.TODO(), metaReq); err != nil {
+			klog.Errorf("err:%+v", err)
+			return err
+		}
 	}
 	return nil
 }
 
-func (o *OpenSergoMetaReporter) openSergoMetaReq(srvInfo *serviceinfo.ServiceInfo) (*v1.ReportMetadataRequest, error) {
-	serviceDesc := &v1.ServiceDescriptor{
-		Name: srvInfo.ServiceName,
-	}
-	streaming := false
-	isStreaming, exist := srvInfo.Extra["streaming"].(bool)
-	if exist {
-		streaming = isStreaming
-	}
-	for methodName, method := range srvInfo.Methods {
-		serviceDesc.Methods = append(serviceDesc.Methods, &v1.MethodDescriptor{
-			Name:            srvInfo.GetPackageName() + "." + methodName,
-			InputTypes:      []string{util.ParamTypeName(method.NewArgs())},
-			OutputTypes:     []string{util.ParamTypeName(method.NewResult())},
-			ClientStreaming: &streaming,
-			ServerStreaming: &streaming,
-		})
-	}
+func (o *OpenSergoMetaReporter) openSergoMetaReq(svcs map[string]*serviceinfo.ServiceInfo) ([]*v1.ReportMetadataRequest, error) {
+	var metaReqs []*v1.ReportMetadataRequest
+	for _, srvInfo := range svcs {
+		serviceDesc := &v1.ServiceDescriptor{
+			Name: srvInfo.ServiceName,
+		}
+		streaming := false
+		isStreaming, exist := srvInfo.Extra["streaming"].(bool)
+		if exist {
+			streaming = isStreaming
+		}
+		for methodName, method := range srvInfo.Methods {
+			serviceDesc.Methods = append(serviceDesc.Methods, &v1.MethodDescriptor{
+				Name:            srvInfo.GetPackageName() + "." + methodName,
+				InputTypes:      []string{util.ParamTypeName(method.NewArgs())},
+				OutputTypes:     []string{util.ParamTypeName(method.NewResult())},
+				ClientStreaming: &streaming,
+				ServerStreaming: &streaming,
+			})
+		}
 
-	serviceMetadata := v1.ServiceMetadata{
-		ServiceContract: &v1.ServiceContract{
-			Services: []*v1.ServiceDescriptor{serviceDesc},
-		},
-	}
-
-	addr, ok := srvInfo.Extra["address"].(net.Addr)
-	if ok {
-		host, port, err := net.SplitHostPort(addr.String())
-		if err != nil {
-			return nil, err
-		}
-		p, err := strconv.Atoi(port)
-		if err != nil {
-			return nil, fmt.Errorf("parse port error: %+v", err)
-		}
-		if host == "" || host == "::" {
-			host, err = util.GetLocalIpv4Host()
-			if err != nil {
-				return nil, fmt.Errorf("parse host error: %+v", err)
-			}
-		}
-		serviceMetadata.ListeningAddresses = []*v1.SocketAddress{
-			{
-				Address:   host,
-				PortValue: uint32(p),
+		serviceMetadata := v1.ServiceMetadata{
+			ServiceContract: &v1.ServiceContract{
+				Services: []*v1.ServiceDescriptor{serviceDesc},
 			},
 		}
+
+		addr, ok := srvInfo.Extra["address"].(net.Addr)
+		if ok {
+			host, port, err := net.SplitHostPort(addr.String())
+			if err != nil {
+				return nil, err
+			}
+			p, err := strconv.Atoi(port)
+			if err != nil {
+				return nil, fmt.Errorf("parse port error: %+v", err)
+			}
+			if host == "" || host == "::" {
+				host, err = util.GetLocalIpv4Host()
+				if err != nil {
+					return nil, fmt.Errorf("parse host error: %+v", err)
+				}
+			}
+			serviceMetadata.ListeningAddresses = []*v1.SocketAddress{
+				{
+					Address:   host,
+					PortValue: uint32(p),
+				},
+			}
+		}
+
+		if protocol, ok := protocolMap[srvInfo.PayloadCodec]; ok {
+			serviceMetadata.Protocols = []string{protocol}
+		}
+
+		metaReq := &v1.ReportMetadataRequest{
+			AppName:         srvInfo.ServiceName,
+			ServiceMetadata: []*v1.ServiceMetadata{&serviceMetadata},
+		}
+		metaReqs = append(metaReqs, metaReq)
 	}
 
-	if protocol, ok := protocolMap[srvInfo.PayloadCodec]; ok {
-		serviceMetadata.Protocols = []string{protocol}
-	}
-
-	return &v1.ReportMetadataRequest{
-		AppName:         srvInfo.ServiceName,
-		ServiceMetadata: []*v1.ServiceMetadata{&serviceMetadata},
-	}, nil
+	return metaReqs, nil
 }
